27d2a3f78a720a49a7168a7999e960f81d828e90
jmmauricio <jmmauricio6@gmail.com>
2022-04-26 18:41:16 +0000

    first enviroment DAE working

---
+++ pendulum/envs/__init__.py
@@ -0,0 +1,3 @@
+"""Pendulum DAE Enviornments."""
+
+from pendulum.envs.pendulum_env import PendulumDAEEnv
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Third party
+from setuptools import setup
+
+setup(
+     name='pendulum',
+   version='0.1.8',
+    install_requires=['pydae'],
+)
